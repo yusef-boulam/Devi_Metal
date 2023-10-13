63fcdd597249d7325d38e02aa36c4a9edac58efb
Yusef Boulam <yusef.boulam@gmail.com>
2023-10-13 23:20:53 +0200

    added photos of the marquee projects

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -94,68 +94,8 @@ export class ProjectsService {
       page: 'projectB',
       images: [
         {
-          src: '../../assets/images/project/Barriere/covers.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/1.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/2.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/3.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/4.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/5.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/6.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/7.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/8.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/9.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/10.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/11.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/12.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/13.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/14.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/15.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/2.jpg',
+          alt: 'photo chapiteau',
         },
 
         // ... autres images
@@ -173,71 +113,130 @@ export class ProjectsService {
       page: 'projectC',
       images: [
         {
-          src: '../../assets/images/project/Barriere/covers.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/covers.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/1.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/2.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/2.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/6.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/3.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/7.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/4.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/8.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/5.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/9.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/6.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/11.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/7.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/13.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/8.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/14.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/9.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/15.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/10.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/16.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/11.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/17.jpg',
+          alt: 'photo chapiteau',
         },
         {
-          src: '../../assets/images/project/Barriere/12.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/13.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/14.jpg',
-          alt: 'photo barriere',
-        },
-        {
-          src: '../../assets/images/project/Barriere/15.jpg',
-          alt: 'photo barriere',
+          src: '../../assets/images/project/Chapiteau/18.jpg',
+          alt: 'photo chapiteau',
         },
 
-        // ... autres images
+        {
+          src: '../../assets/images/project/Chapiteau/19.jpg',
+          alt: 'photo chapiteau',
+        },
+
+        {
+          src: '../../assets/images/project/Chapiteau/20.jpg',
+          alt: 'photo chapiteau',
+        },
+
+        {
+          src: '../../assets/images/project/Chapiteau/21.jpg',
+          alt: 'photo chapiteau',
+        },
+
+        {
+          src: '../../assets/images/project/Chapiteau/22.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/24.jpg',
+          alt: 'photo chapiteau',
+        },
+
+        {
+          src: '../../assets/images/project/Chapiteau/25.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/26.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/27.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/28.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/29.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/30.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/32.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/33.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/34.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/35.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/37.jpg',
+          alt: 'photo chapiteau',
+        },
+        {
+          src: '../../assets/images/project/Chapiteau/38.jpg',
+          alt: 'photo chapiteau',
+        },
       ],
     },
   ];
